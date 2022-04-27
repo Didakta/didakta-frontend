540f1681a900c7bf2b71b938e97784294217c9b4
AlienCoder <37622754+mrclcyn@users.noreply.github.com>
2022-04-27 13:47:35 +0200

    conflicts resolved

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,9 @@
-import React from "react";
-import { Container } from "@mui/material";
-import { Typography } from "@mui/material";
-import { Link } from "@mui/material";
 import logo from "../images/icon-red.png";
-import { Box } from "@mui/system";
-import { Card } from "@mui/material";
-import { CardActions } from "@mui/material";
-import { Button } from "@mui/material";
-import "../styles/mui_styles.css";
-import "../styles/home.css";
+import { Link } from "react-router-dom";
 
 export default function About() {
   return (
-    <Container
-      className="MuiContainer-root-MuiContainer-maxWidthSm-css-kae4yu-MuiContainer-root"
-      component="main"
-      sx={{ mt: 8, mb: 2, pl: 1 }}
-      maxWidth="sm"
-      justifyContent="center"
-    >
-      {/* <Card sx={{ pl:5, pr: 5, pt:5, pb:-10 }} bgColor="#eceff1" >  */}
+    <div>
       <Link to="/register">
         <img
           id="Logo"
@@ -31,22 +15,8 @@ export default function About() {
         />
       </Link>
 
-      {/* <Typography variant="body1" justifyContent="center" marginTop="50px"></Typography> */}
-      <Typography
-        variant="h4"
-        component="h1"
-        gutterBottom
-        justifyContent="center"
-      >
-        About DIDAKTA
-      </Typography>
-      <Typography
-        className="MuiTypography-root-MuiTypography-h5-MuiTypography-gutterBottom-css-xwdu4f-MuiTypography-root"
-        variant="h6"
-        component="h2"
-        gutterBottom
-        justifyContent="center"
-      >
+      <h1>About DIDAKTA</h1>
+      <div>
         <p>
           Didakta does not use non-authentic texts. Every text and exercise in
           Didakta is taken from the selected corpus and you will be reading
@@ -65,13 +35,7 @@ export default function About() {
           Implemented by Arsalan Moharrebi and Deborah Wright
         </p>
         <p>We’re thankful for the invaluable contributions of Tariq Yousef</p>
-      </Typography>
-      {/* <Typography variant="body1" justifyContent="center" sx={{ pl: 23 }}>Click to register or login.</Typography> */}
-
-      {/* <Box backgroundColor="#eceff1">   */}
-
-      {/* </Card> */}
-      {/* </Box> */}
-    </Container>
+      </div>
+    </div>
   );
 }
